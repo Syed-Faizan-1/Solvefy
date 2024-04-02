be5287e8959641f009cb845aab08d09c34dcea92
Syed-Faizan-1 <faaiz290302@gmail.com>
2024-04-02 16:28:45 +0500

    Add Get All Products Functionality in ProductController & Display All Products in the View & Some Styling

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,12 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Product_Inventory.Data;
 
 namespace Product_Inventory.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            var productList = _db.Products.ToList();
+            return View(productList);
         }
     }
 }
