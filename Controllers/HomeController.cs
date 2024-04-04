62883de0ed860515681e65167333033f514017d1
Syed-Faizan-1 <faaiz290302@gmail.com>
2024-04-04 06:03:17 +0500

    Add Products to Home Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ProductInventory.Data;
 using ProductInventory.Models;
 using System.Diagnostics;
 
@@ -7,15 +8,18 @@ namespace ProductInventory.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _db.Products;
+            return View(productList);
         }
 
         public IActionResult Privacy()
